b7b1ffa5e9fc424d9cb6057d23480cf46a779d83
Radek Micek <radek.micek@gmail.com>
2024-12-10 12:43:01 +0100

    Add example

---
+++ libs/sokol/sokol.c
@@ -0,0 +1,18 @@
+#define SOKOL_IMPL
+
+#include "sokol_defines.h"
+
+#include "sokol_audio.h"
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_log.h"
+#include "sokol_time.h"
+#include "sokol_glue.h"
+
+#include "sokol_gl.h"
+#include "sokol_shape.h"
+#include "sokol_debugtext.h"
+
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "cimgui.h"
+#include "sokol_imgui.h"
