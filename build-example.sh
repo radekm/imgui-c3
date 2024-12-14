5dc38e157d101ef49bb8d6d26a542d723888d7cb
Radek Micek <radek.micek@gmail.com>
2024-12-14 22:06:04 +0100

    Fix compilation to dynamic library on macOS

--- build-example.sh
+++ build-example.sh
@@ -7,4 +7,4 @@ build_dir="$script_dir/build"
 mkdir -p "$build_dir"
 cd "$build_dir"
 
-c3c compile -O1 -L "$build_dir/libs" --libdir .. --lib sokol --lib imgui ../example/*.c3 -o example
+c3c compile -O1 --macos-min-version 13.0 -L "$build_dir/libs" --libdir .. --lib sokol --lib imgui ../example/*.c3 -o example
